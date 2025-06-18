408a82807e0b78bfd5d2a44d616666a37be73bea
Suraj Biswal <114858523+SurajBiswal@users.noreply.github.com>
2025-06-18 15:44:09 +0530

    Time: 1 ms (29.35%), Space: 44.4 MB (61.97%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -13,23 +13,45 @@
  *     }
  * }
  */
-class Solution {
-    private int node=-1;
-    private int count=0;
-    public int kthSmallest(TreeNode root, int k) {
-        helper(root, k);
-        return node;
-    }
+// class Solution {
+//     private int node=-1;
+//     private int count=0;
+//     public int kthSmallest(TreeNode root, int k) {
+//         helper(root, k);
+//         return node;
+//     }
 
-    public boolean helper(TreeNode root, int k) {
-        if(root==null)return false;
-        helper(root.left, k);
-        ++count;
-        if(k==count){
-            node = root.val;
-            return true;
-        } 
-        helper(root.right, k);
-        return false;
+//     public boolean helper(TreeNode root, int k) {
+//         if(root==null)return false;
+//         helper(root.left, k);
+//         ++count;
+//         if(k==count){
+//             node = root.val;
+//             return true;
+//         } 
+//         helper(root.right, k);
+//         return false;
+//     }
+// }
+
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+
+    Stack<TreeNode>st=new Stack<>();
+
+    while(root!=null || !st.isEmpty()){
+        while(root!=null){
+            st.push(root);
+            root=root.left;
+        }
+
+        root = st.pop();
+        k--;
+        if(k==0) return root.val;
+
+        root = root.right;
+    }
+    
+    return -1;
     }
 }
