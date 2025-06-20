2e65e0e79acd3d60973185396745060a394ca4ac
Suraj Biswal <114858523+SurajBiswal@users.noreply.github.com>
2025-06-20 11:21:36 +0530

    Time: 41 ms (11.91%), Space: 45.9 MB (9.39%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -6,24 +6,21 @@ class Solution {
         }
         Arrays.sort(coins);
         int ans = helper(0, coins, amount, dp);
-        if(ans==Integer.MAX_VALUE) return -1;
-        return ans;
+        return ans==Integer.MAX_VALUE ? -1 : ans;
     }
     public int helper(int ind, int[] coins, int amount, int[][]dp){
         if(amount==0) return 0;
-        if(ind==coins.length || coins[ind]> amount) return Integer.MAX_VALUE;
+        if(ind==coins.length) return Integer.MAX_VALUE;
         if(dp[ind][amount]!=-1)return dp[ind][amount];
 
-        int pickSame =Integer.MAX_VALUE;
-        int path1 = helper(ind, coins, amount-coins[ind], dp);
-        if(path1 != Integer.MAX_VALUE) pickSame=1+path1;
-
         int pick = Integer.MAX_VALUE;
-        int path2 = helper(ind+1, coins, amount-coins[ind], dp);
-        if(path2 != Integer.MAX_VALUE) pick=1+path2;
+        if(amount >= coins[ind]){
+            int path1 = helper(ind, coins, amount-coins[ind], dp);
+            if(path1 != Integer.MAX_VALUE) pick = 1+path1;
+        }
 
         int notPick = helper(ind+1, coins, amount, dp);
 
-        return dp[ind][amount]= Math.min(pickSame, Math.min(pick, notPick));
+        return dp[ind][amount]= Math.min(pick, notPick);
     }
 }
