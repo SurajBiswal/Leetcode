8cd44e504669bc0d24c30de525fb972ffdf46761
Suraj Biswal <114858523+SurajBiswal@users.noreply.github.com>
2025-06-20 11:21:42 +0530

    [322] [Time Beats: 11.91%] [Memory Beats: 9.39%] - LeetPush

--- Coin-Change.java
+++ Coin-Change.java
@@ -1,25 +1,26 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if(amount==0)return 0;
-        int n = coins.length;
-        int[][]dp = new int[n+1][amount+1];
-        for(int []row:dp){
-            Arrays.fill(row,-1);
+        int[][]dp = new int[coins.length][amount+1];
+        for(int i=0;i<coins.length;i++){
+            Arrays.fill(dp[i],-1);
         }
-        int ans =  helper(coins, n-1, amount, dp);
-        if(ans == Integer.MAX_VALUE) return -1;
-        return ans;
+        Arrays.sort(coins);
+        int ans = helper(0, coins, amount, dp);
+        return ans==Integer.MAX_VALUE ? -1 : ans;
     }
-    public static int helper(int[] coins,int i,int amt, int[][]dp){
-        if(amt==0)return 0;
-        if(i<0) return Integer.MAX_VALUE;
-        if(dp[i][amt] != -1) return dp[i][amt];
+    public int helper(int ind, int[] coins, int amount, int[][]dp){
+        if(amount==0) return 0;
+        if(ind==coins.length) return Integer.MAX_VALUE;
+        if(dp[ind][amount]!=-1)return dp[ind][amount];
+
         int pick = Integer.MAX_VALUE;
-        if(amt>=coins[i]){
-            int res = helper(coins, i, amt-coins[i], dp);
-            if(res != Integer.MAX_VALUE) pick = res+1;
+        if(amount >= coins[ind]){
+            int path1 = helper(ind, coins, amount-coins[ind], dp);
+            if(path1 != Integer.MAX_VALUE) pick = 1+path1;
         }
-        int not_pick = helper(coins, i-1, amt, dp);
-        return dp[i][amt] = Math.min(pick, not_pick);
+
+        int notPick = helper(ind+1, coins, amount, dp);
+
+        return dp[ind][amount]= Math.min(pick, notPick);
     }
 }
