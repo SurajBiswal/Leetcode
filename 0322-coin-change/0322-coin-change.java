e7827532d87de5b342764bded60591108414bdec
Suraj Biswal <114858523+SurajBiswal@users.noreply.github.com>
2025-07-21 01:10:45 +0530

    Time: 40 ms (10.8%), Space: 45.4 MB (17.82%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,26 +1,26 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[][]dp = new int[coins.length][amount+1];
-        for(int i=0;i<coins.length;i++){
-            Arrays.fill(dp[i],-1);
-        }
         Arrays.sort(coins);
-        int ans = helper(0, coins, amount, dp);
-        return ans==Integer.MAX_VALUE ? -1 : ans;
+        int i=0,j=coins.length-1;
+        int [][]dp = new int[coins.length][amount+1];
+        for(int []row:dp)Arrays.fill(row, -1);
+        int ans = calcCombo(coins, amount, 0, dp);
+        if(ans < Integer.MAX_VALUE) return ans;
+        return -1;
     }
-    public int helper(int ind, int[] coins, int amount, int[][]dp){
-        if(amount==0) return 0;
-        if(ind==coins.length) return Integer.MAX_VALUE;
-        if(dp[ind][amount]!=-1)return dp[ind][amount];
+    public int calcCombo(int[]coins, int amt, int ind, int[][]dp){
 
-        int pick = Integer.MAX_VALUE;
-        if(amount >= coins[ind]){
-            int path1 = helper(ind, coins, amount-coins[ind], dp);
-            if(path1 != Integer.MAX_VALUE) pick = 1+path1;
-        }
+        if(amt==0) return 0;
+        if(ind >= coins.length || amt < 0) return Integer.MAX_VALUE;
 
-        int notPick = helper(ind+1, coins, amount, dp);
+        if(dp[ind][amt] != -1) return dp[ind][amt];
 
-        return dp[ind][amount]= Math.min(pick, notPick);
+        int pick = calcCombo(coins, amt-coins[ind], ind, dp);
+        if(pick < Integer.MAX_VALUE) pick+=1;
+
+        int notPick = calcCombo(coins, amt, ind+1, dp);
+
+        return dp[ind][amt] = Math.min(pick, notPick);
     }
 }
+
