6b797af771dd3309508492c5b2fad2c8b1c1bb5e
Suraj Biswal <114858523+SurajBiswal@users.noreply.github.com>
2025-08-31 11:26:34 +0530

    Time: 1 ms (100%), Space: 46.4 MB (12.3%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -8,17 +8,21 @@ class Solution {
         double mid1=0,mid2=0;
         int i=0, j=0, k=0;
         while(i<n1 && j<n2){
-            if(nums1[i]<=nums2[j]){
-                if(k==m1) mid1=nums1[i];
-                if(size%2 == 0 && k==m2) mid2=nums1[i];
-                i++;
-                k++;
-            }else{
-                if(k==m1) mid1=nums2[j];
-                if(size%2 == 0 && k==m2) mid2=nums2[j];
-                j++;
-                k++;
-            }
+            // if(nums1[i]<=nums2[j]){
+            //     if(k==m1) mid1=nums1[i];
+            //     if(size%2 == 0 && k==m2) mid2=nums1[i];
+            //     i++;
+            //     k++;
+            // }else{
+            //     if(k==m1) mid1=nums2[j];
+            //     if(size%2 == 0 && k==m2) mid2=nums2[j];
+            //     j++;
+            //     k++;
+            // }
+            int val = nums1[i]<=nums2[j] ? nums1[i++]:nums2[j++];
+            if(k==m1) mid1=val;
+            if(k==m2) mid2=val;
+            k++;
         }
         while(i<n1){
             if(k==m1) mid1=nums1[i];
