0b84ecadb01706d4567701458bc260b4b0f6f2f1
Suraj Biswal <114858523+SurajBiswal@users.noreply.github.com>
2025-09-28 10:00:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0018-4sum/0018-4sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        int n = nums.length;
+        List<List<Integer>>ans=new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i=0;i<n-3;i++){
+            for(int j=i+1;j<n-2;j++){
+                int sum = nums[i]+nums[j];
+                int s=j+1;
+                int e=n-1;
+                while(s<e){
+                    long tempSum = sum+nums[s]+nums[e];
+                    if(tempSum == target){
+                        if(!ans.contains(Arrays.asList(nums[i],nums[j],nums[s],nums[e]))){
+                            ans.add(Arrays.asList(nums[i],nums[j],nums[s],nums[e]));
+                        }
+                        s++;
+                    }else if(tempSum > target){
+                        e--;
+                    }else{
+                        s++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
+
