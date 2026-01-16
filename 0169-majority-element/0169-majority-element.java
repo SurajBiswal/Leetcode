057fd17fdfb10a27821d15771056bd156022f54e
Suraj Biswal <114858523+SurajBiswal@users.noreply.github.com>
2026-01-16 23:23:34 +0530

    Time: 2 ms (72.31%), Space: 55.7 MB (36.97%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,20 +1,20 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
-        int ans = nums[0];
-        int freq =1;
-        for(int i=1;i<n;i++){
-            if(nums[i]==ans){
-                freq++;
+        int max = nums[0];
+        int cnt = 1;
+
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==max){
+                cnt++;
             }else{
-                freq--;
+                cnt--;
             }
-            if(freq==0){
-                ans = nums[i];
-                freq = 1;
+            if(cnt<0){
+                cnt=1;
+                max = nums[i];
             }
         }
-        return ans;
+
+        return max;
     }
-    
 }
