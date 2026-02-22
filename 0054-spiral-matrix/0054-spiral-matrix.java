35e1eddc92d240d9877a5a46c49e3a69511ba895
Suraj Biswal <114858523+SurajBiswal@users.noreply.github.com>
2026-02-22 10:48:23 +0530

    Time: 0 ms (100%), Space: 43.2 MB (41.22%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,43 +1,35 @@
 class Solution {
-    public List<Integer> spiralOrder(int[][] mat) {
-        ArrayList<Integer>A = new ArrayList<>();
-        
-        int m=mat.length  , n=mat[0].length;
-        int minr=0 , minc=0;
-        int maxr=m-1 , maxc=n-1;
-        int count =1  ,   size=m*n;
-        
-        while(count<=size){
-            //1st loop=> i=minr & j=minc to maxc
-            for(int i=minr , j=minc ; j<=maxc && count<=size ;j++){
-                A.add(mat[i][j]);
-                count++;
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int rs = 0;              
+        int re = matrix.length-1;    
+        int cs = 0;           
+        int ce = matrix[0].length-1; 
+        List<Integer>list = new ArrayList<>();
+        while(rs <= re && cs <= ce){
+            for(int i=cs;i<=ce;i++){
+                list.add(matrix[rs][i]);
             }
-            minr++;          //increment the constant in this loop
+            rs+=1;
             
-        //2nd loop=> j=maxc & i=minr to maxr
-            for(int i=minr,j=maxc ; i<=maxr && count<=size ; i++){
-                A.add(mat[i][j]);
-                count++;
+            for(int i=rs;i<=re;i++){
+                list.add(matrix[i][ce]);
             }
-            maxc--;
-            
-            //3rd loop=> i=maxr && j=maxc to minc
-            for(int i=maxr , j=maxc ; j>=minc && count<=size ; j--){
-                A.add(mat[i][j]);
-                count++;
+            ce-=1;
+            if(rs<=re){
+                for(int i=ce;i>=cs;i--){
+                    list.add(matrix[re][i]);
+                }
+                re-=1;
             }
-            maxr--;
-            
-    //4th loop=> i=maxr to minr && j=minc
-            for(int i=maxr,j=minc ; i>=minr && count<=size; i--){
-                A.add(mat[i][j]);
-                count++;
+
+            if(cs<=ce){
+                for(int i=re;i>=rs;i--){
+                    list.add(matrix[i][cs]);
+                }
+                cs+=1;
             }
-            minc++;
-            
         }
-        
-        return A;
+
+        return list;
     }
 }
